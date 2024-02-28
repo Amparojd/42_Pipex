5f80f6e3ec9d5f8a085014944846ee4b19de4dca
Amparo Jiménez <amparo_jd@hotmail.com>
2024-02-28 22:26:12 +0100

    string_output ok

--- Makefile
+++ Makefile
@@ -1,23 +1,17 @@
 NAME= pipex.a
 SRC= ./srcs/search_path.c ./srcs/utils.c ./pipex.c \
 	./srcs/ft_split.c ./srcs/ft_strchr.c ./srcs/ft_strlen.c \
-	./srcs/ft_strlcpy.c ./srcs/ft_putstr_fd.c ./srcs/ft_putendl_fd.c \
-	./srcs/ft_putchar_fd.c ./srcs/ft_strlcat.c ./srcs/ft_strjoin.c ./srcs/ft_strncmp.c
+	./srcs/ft_strlcpy.c \
+	./srcs/string_output.c ./srcs/ft_strlcat.c ./srcs/ft_strjoin.c ./srcs/ft_strncmp.c
 
 FLAGS= gcc -Wextra -Werror -Wall
-OBJ1=$(SRC:.c=.o)
-OBJ=$(OBJ1:.m=.o)
+OBJ=$(SRC:.c=.o)
 
 $(NAME):	$(OBJ)
 	@clear
 	@echo "\n\033[33m"🛠️⏳ Compiling... Patience is a virtue! 😊⏳🛠️"\033[0m"
 	@sleep 3
-
-# @make -sC ./libft/
-# @make bonus -sC ./libft/
-# @cp libft/libft.a .
 	@ar -rc $(NAME) $(OBJ)
-	@ranlib $(NAME)
 	@clear
 	@echo "\n\033[32m"✨🎉 Compilation successful! All set to go! 🚀✨"\033[0m\n"
 	@$(FLAGS) $(SRC) -o pipex
--- srcs/string_output.c
+++ srcs/string_output.c
@@ -1,19 +1,38 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putendl_fd.c                                    :+:      :+:    :+:   */
+/*   ft_putchar_fd.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ampjimen <ampjimen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/02/26 17:49:45 by ampjimen          #+#    #+#             */
-/*   Updated: 2024/02/26 17:49:48 by ampjimen         ###   ########.fr       */
+/*   Created: 2024/02/26 15:45:33 by ampjimen          #+#    #+#             */
+/*   Updated: 2024/02/26 15:45:38 by ampjimen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/pipex.h"
 
+void	ft_putchar_fd(char c, int fd)
+{
+	write(fd, &c, 1);
+}
+
 void	ft_putendl_fd(char *s, int fd)
 {
 	ft_putstr_fd(s, fd);
 	ft_putchar_fd('\n', fd);
+}
+
+void	ft_putstr_fd(char *s, int fd)
+{
+	size_t	i;
+
+	i = 0;
+	if (s == NULL)
+		return ;
+	while (s[i] != '\0')
+	{
+		ft_putchar_fd(s[i], fd);
+		i++;
+	}
 }
